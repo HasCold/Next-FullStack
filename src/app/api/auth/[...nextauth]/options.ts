a8971de0bbe0c0a1211d2e234e5245263f3bbf91
Hasan <ha03330224926@gmail.com>
2024-08-11 14:46:49 +0500

    Intialize the Next-Auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,51 @@
+import connectDB from "@/config/db";
+import UserModel from "@/models/user.Model";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from 'bcryptjs';
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                username: { label: "Email", type: "text"},
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials: any): Promise<any>{
+                await connectDB();
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            {username: credentials.identifier},
+                            {email: credentials.identifier},
+                        ]
+                    });
+
+                    if(!user) throw new Error("No user found with these email");
+                    if(!user.isVerified) throw new Error("Please verify your account before login");
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+                    if(isPasswordCorrect){
+                        return user;
+                    }else{
+                        throw new Error("Incorrect Password");
+                    }
+
+                } catch (err: any) {
+                    throw new Error(err)
+                }
+
+            }
+        })
+    ], 
+    pages: {
+        signIn: "/sign-in"  // we have given the control of sign-in route to the Next-Auth, so next-auth will handle all the thing for us 
+    },
+    session: {
+        strategy: "jwt"
+    }, 
+    secret: process.env.NEXTAUTH_SECRET
+} 
